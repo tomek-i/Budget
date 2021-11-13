39b40596fd4b0626bbd508dc31ae33d175accb19
Tomek <tomek.iwainski@gmail.com>
2021-11-13 12:07:52 +1100

    storybook

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,10 +1,34 @@
+const path = require("path");
+
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+    "@storybook/addon-essentials",
+    "storybook-css-modules-preset",
+  ],
+  //presets: [path.resolve(__dirname, "./next-preset.js")],
+};
+
+// module.exports = {
+//   stories: [
+//     "../stories/**/*.stories.mdx",
+//     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+//   ],
+//   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+//   webpackFinal: async (config, { configType }) => {
+//     config.module.rules.push({
+//       test: /\.css$/,
+//       use: [
+//         {
+//           loader: "postcss-loader",
+//         },
+//       ],
+//       include: path.resolve(__dirname, "../"),
+//     });
+//     return config;
+//   },
+// };
--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,3 +1,6 @@
+// .storybook/preview.js
+import "../styles/globals.css";
+import * as nextImage from "next/image";
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +9,31 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
+
+// Replace next/image for Storybook
+Object.defineProperty(nextImage, "default", {
+  configurable: true,
+  value: (props) => {
+    const { width, height } = props;
+    const ratio = (height / width) * 100;
+    return (
+      <div
+        style={{
+          paddingBottom: `${ratio}%`,
+          position: "relative",
+        }}
+      >
+        <img
+          style={{
+            objectFit: "cover",
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+          }}
+          {...props}
+        />
+      </div>
+    );
+  },
+});
