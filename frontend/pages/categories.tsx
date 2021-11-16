6f866d3ffd8db414f28e949ae65ee65816854eed
Tomek <tomek.iwainski@gmail.com>
2021-11-17 00:48:55 +1100

    refactored

--- frontend/pages/categories.tsx
+++ frontend/pages/categories.tsx
@@ -7,7 +7,8 @@ import axios from 'axios';
 import { CategoryType } from '../../common/types/category.type';
 import { TransactionType } from '../../common/types/transaction.type';
 
-class Category implements CategoryType {
+export class Category implements CategoryType {
+  id!: string;
   title!: string;
   description?: string;
   icon?: string;
@@ -23,16 +24,16 @@ class CategoryDisplay {
   constructor(data: Category) {
     this.title = data.title;
     this.description = data.description;
-
-    if (data.icon) {
-      this.icon = `<img width="50" src=${data.icon} alt="${this.title}" />`;
-    }
+    this.icon = data.icon;
+    // if (data.icon) {
+    //   this.icon = `<img width="50" src=${data.icon} alt="${this.title}" />`;
+    // }
   }
 }
 
 const Categories: NextPage = () => {
-  const [categories, setCategories] = useState<Category[]>([]);
-  const [columns, setColumns] = useState<Column<object>[]>([]);
+  const [categories, setCategories] = useState<Category[]>([]); //TODO: use memo?!
+  const [columns, setColumns] = useState<Column<object>[]>([]); //TODO: use memo??
   const [loading, setLoading] = useState(false);
 
   const init = async () => {
@@ -54,26 +55,44 @@ const Categories: NextPage = () => {
         response.data.map((cat) => {
           display.push(new CategoryDisplay(cat));
         });
-        setCategories([display[0]]);
+        setCategories(response.data);
       }
       setLoading(false);
     }
     fetchData();
   }, []);
 
+  const IconCell = ({ values }: { values: string }) => {
+    // Loop through the array and create a badge-like component instead of a comma-separated string
+    return <img src={values} alt={''} />;
+  };
+
   const createColumns = () => {
     let cols: Column<object>[] = Object.getOwnPropertyNames(new Category()).map(
       (columnName) => {
-        return {
+        let cell: Column<object> = {
           Header: columnName.toUpperCase(),
           accessor: columnName,
         };
+
+        //if we are an icon cell then we render differently
+        if (columnName.toLowerCase().includes('icon')) {
+          cell = {
+            ...cell,
+            // Cell method will provide the cell value; we pass it to render a custom component
+            Cell: ({
+              cell: { value },
+            }: {
+              cell: {
+                value: any;
+              };
+            }) => <IconCell values={value} />,
+          };
+        }
+
+        return cell;
       },
     );
-    // cols.push({
-    //   Header: 'Amount',
-    //   accessor: 'amount',
-    // });
     setColumns(cols);
   };
 
