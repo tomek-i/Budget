786758da935fd03bcc5f172d5cbdd208f2e7fdc3
Tomek <tomek.iwainski@gmail.com>
2021-11-17 00:47:43 +1100

    added decorators

---
+++ frontend/decorators/index.ts
@@ -0,0 +1,22 @@
+import { CellProps, Renderer } from 'react-table';
+import 'reflect-metadata';
+
+const tableDisplayColumnKey = Symbol('column');
+
+interface ColumnOptions {
+  visible?: boolean;
+  title?: string;
+  order?: number;
+  Cell?: Function;
+}
+
+export function column(options: ColumnOptions) {
+  return Reflect.metadata(tableDisplayColumnKey, options);
+}
+
+export function getColumnOptions(
+  target: any,
+  propertyKey: string,
+): ColumnOptions {
+  return Reflect.getMetadata(tableDisplayColumnKey, target, propertyKey);
+}
